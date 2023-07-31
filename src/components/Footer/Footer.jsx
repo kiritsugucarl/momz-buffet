bb04d1bec870c35abd3018524f3baad41d608754
Kiritsugucarl <bluesniper017@gmail.com>
2023-07-31 22:36:03 +0800

    finish: navbar, footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,16 @@
-
+import './Footer.css'
 
 const Footer = () => {
     return(
         <footer className="footer fullwidth section">
-            <div className="footer__grid-2">
-                <div className="footer__grid-column">
-                    <p className="footer__p">Mock-up Website made by: Carl Dennis Pineda</p>
-                    <p className="footer__p">Stack used: HTML, SCSS, JS</p>
-                </div>
-                <div className="footer__grid-column">
-                    <p className="footer__p">Got Questions? Contact me with:</p>
-                    <p className="footer__p">Email : cdbpineda02@gmail.com</p>
-                    <p className="footer__p">Github : kiritusugcarl</p>
-                </div>
+            <div className="footer__content-column">
+                <p className="footer__p">Mock-up Website made by: Carl Dennis Pineda</p>
+                <p className="footer__p">Stack used: HTML, SCSS, JS</p>
+            </div>
+            <div className="footer__content-column">
+                <p className="footer__p">Got Questions? Contact me with:</p>
+                <p className="footer__p">Email : cdbpineda02@gmail.com</p>
+                <p className="footer__p">Github : kiritusugcarl</p>
             </div>
         </footer>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
-
+import './Home.css'
 
 const Home = () => {
     return (
         <main className="main container section">
-            <h1>Home</h1>
+            <h1 className="home__title">
+                MOMZ BUFFET & <br/>
+                FOOD SERVICES
+            </h1>
+            <p>
+                <span className="home__strong-1">Friday Nights</span>,
+                <span className="home__strong-2">Weekend Outings</span>,
+                <span className="home__strong-3">Birthday Celebrations</span>
+                <span className="home__strong-4">hanging out with your friends</span>
+            </p>
         </main>
     )
 }
